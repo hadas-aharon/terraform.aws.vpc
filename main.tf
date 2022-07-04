0b78c2b143558fad60e8d25dc78ddb84116ececf
hadas-aharon <100468500+hadas-aharon@users.noreply.github.com>
2022-07-04 13:47:58 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,124 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+########################################Variables###########################
+
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "eu-central-1a"
+}
+variable "public_key_path" {
+  description = "Public key path"
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-0a1ee2fb28fe05df3"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Production"
+}
+########################################Resources################################
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.cidr_vpc}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.cidr_subnet}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${var.availability_zone}"
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+resource "aws_route_table" "rtb_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = "${aws_subnet.subnet_public.id}"
+  route_table_id = "${aws_route_table.rtb_public.id}"
+}
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Environment = "${var.environment_tag}"
+  }
+}
+##################################creat public key##################
+resource "aws_key_pair" "ec2key" {
+  key_name = "ec2key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+  }
+  
+################################save privet key localy################
+  resource "local_file" "ec2key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "ec2TFkey.pem"
+}
+
+#################################creat ec2########################
+resource "aws_instance" "testInstance" {
+  ami           = "${var.instance_ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+  key_name = "ec2key"
+ tags = {
+  Environment = "${var.environment_tag}"
+  Name = "Demo"
+ }
+}
+
